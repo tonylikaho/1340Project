612c5b346cf646042b42f430f20f984105c82ff2
3035572296 <49070317+3035572296@users.noreply.github.com>
2019-04-21 21:54:49 +0800

    Update projectcpp.cpp

--- projectcpp.cpp
+++ projectcpp.cpp
@@ -1,7 +1,35 @@
 
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include<sstream>
 using namespace std;
 
-int main(){
-return 0;
+struct Info{
+  int ID, age, salary;
+  string name, role;
+}
+
+
+ifstream input ("data.txt");
+ofstream output ("data.txt");
+
+void GetData(){
+  string str, line;
+  int i=0;
+  while (getline(input,line)){
+    istringstream temp(line);
+    temp>>Staffs
+
+  
+int Main(){
+  int num_of_staff=0, num_of_datatypes=5;
+  cou>>num_of_staff>>num_of_datatypes;
+      
+  Info * staff = new Info [num_of_staff];
+  GetData(
+  
+  return 0;
 }
